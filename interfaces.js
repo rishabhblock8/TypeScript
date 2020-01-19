0fea9eb9ad003e3ff59a6be1169795ebfc49b65c
rishabhblock8 <59728125+rishabhblock8@users.noreply.github.com>
2020-01-20 00:03:54 +0530

    Add files via upload

--- interfaces.js
+++ interfaces.js
@@ -1,11 +1,15 @@
 var oldCivic = {
     name: 'civic',
     year: 2000,
-    broken: true
+    broken: true,
+    summary: function () {
+        return "Name: " + this.name;
+    }
 };
 var printVehicle = function (vehicle) {
-    console.log("Name: " + vehicle.name);
-    console.log("Year: " + vehicle.year);
-    console.log("Broken? " + vehicle.broken);
+    // console.log(`Name: ${vehicle.name}`);
+    // console.log(`Year: ${vehicle.year}`);
+    // console.log(`Broken? ${vehicle.broken}`);
+    console.log(vehicle.summary());
 };
 printVehicle(oldCivic);
