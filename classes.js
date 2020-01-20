5c884c72632d74f414699416586d986ac21972a9
rishabhblock8 <59728125+rishabhblock8@users.noreply.github.com>
2020-01-20 14:58:50 +0530

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,41 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Vehicle = /** @class */ (function () {
+    function Vehicle() {
+    }
+    Vehicle.prototype.drive = function () {
+        console.log('chugga chugga');
+    };
+    Vehicle.prototype.honk = function () {
+        console.log('beep');
+    };
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    // override
+    Car.prototype.drive = function () {
+        console.log('vroom');
+    };
+    return Car;
+}(Vehicle));
+// const vehicle = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
+var car = new Car();
+car.drive();
+car.honk();
